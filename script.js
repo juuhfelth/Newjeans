778d9918cd70f22af8951e421914ac261fde4ff2
juuh <143627760+juuhfelth@users.noreply.github.com>
2026-02-15 18:50:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+
+        const targetElement = document.querySelector(targetId);
+
+        targetElement.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+})
